3bd4edc5da8bff12690e1cef43b108e4e849180e
kourbani <kanidoux@gmail.com>
2020-11-12 11:15:06 +0200

    States - onInputChange, onButtonSubmit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
+import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+  // States
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    };
+  }
+  
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
     <div className="App">
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm 
+        onInputChange={this.onInputChange}
+        onButtonSubmit={this.onButtonSubmit}  
+      />
       { /*      
       <FaceRecognition />} */}
     </div>
   );
+  } 
 }
 
 export default App;
