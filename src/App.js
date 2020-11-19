f7cb607c26f3cdcababd317351f889c55f43b9f0
kourbani <kanidoux@gmail.com>
2020-11-19 09:55:38 +0200

    Display recieved user data from Backend in Rank component

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
       { this.state.route === 'home' ?
         <div>
           <Logo />
-          <Rank />
+          <Rank name={this.state.user.name} entries={this.state.user.entries}/>
           <ImageLinkForm 
             onInputChange={this.onInputChange}
             onButtonSubmit={this.onButtonSubmit}
@@ -104,7 +104,7 @@ class App extends Component {
          </div>
         : (
             route === 'signin' ?
-            <SignIn onRouteChange = {this.onRouteChange}/>
+            <SignIn loadUser = {this.loadUser} onRouteChange = {this.onRouteChange}/>
             : <Register loadUser = {this.loadUser} onRouteChange = {this.onRouteChange} />
           )
       }
