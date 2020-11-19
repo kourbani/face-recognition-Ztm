2b5e9b44d0e184d51961958317440e321dc9c406
kourbani <kanidoux@gmail.com>
2020-11-19 09:27:54 +0200

    Connect with Backend - Register

--- src/App.js
+++ src/App.js
@@ -23,8 +23,26 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     };
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
+      }
+    });
   }
 
   calculateFaceLocation = (data) => {
@@ -87,7 +105,7 @@ class App extends Component {
         : (
             route === 'signin' ?
             <SignIn onRouteChange = {this.onRouteChange}/>
-            : <Register onRouteChange = {this.onRouteChange} />
+            : <Register loadUser = {this.loadUser} onRouteChange = {this.onRouteChange} />
           )
       }
     </div>
